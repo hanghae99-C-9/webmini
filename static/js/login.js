6649255808eaeec2cd5a842543f466448d321bc0
HyeongseopLee <hs421@naver.com>
2022-05-11 20:59:35 +0900

    중복확인 함수 실행 시 새로고침 이벤트 제어
    회원 가입 후 모든 인풋의 값과 도움말 텍스트들을 초기화

--- static/js/login.js
+++ static/js/login.js
@@ -1,5 +1,6 @@
 "use strict";
-
+const checkUpBtn = document.getElementById("checkUpBtn");
+const signupBtn = document.getElementById("signupBtn");
 // 정규식
 function is_nickname(asValue) {
   var regExp = /^(?=.*[a-zA-Z])[-a-zA-Z0-9_.]{2,10}$/;
@@ -11,7 +12,11 @@ function is_password(asValue) {
 }
 
 // username 중복 확인
-function check_dup() {
+// function check_dup(e) {
+//   event.preventDefault();
+// }
+checkUpBtn.addEventListener("click", function (event) {
+  event.preventDefault();
   let username = $("#input-username").val();
   if (username == "") {
     $("#help-id")
@@ -23,9 +28,7 @@ function check_dup() {
   }
   if (!is_nickname(username)) {
     $("#help-id")
-      .text(
-        "아이디의 형식을 확인해주세요. 영문과 숫자, 일부 특수문자(._-) 사용 가능. 2-10자 길이"
-      )
+      .text("영문과 숫자, 일부 특수문자(._-) 사용 가능. 2-10자 길이")
       .removeClass("is-safe")
       .addClass("is-danger");
     $("#input-username").focus();
@@ -54,81 +57,83 @@ function check_dup() {
       $("#help-id").removeClass("is-loading");
     },
   });
-}
+});
 
 // signin 버튼 클릭 후
 // 회원가입 조건이 충족 되었을 때
 // login페이지로 넘어감
-document
-  .getElementById("signupBtn")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    let username = $("#input-username").val();
-    let password = $("#input-password").val();
-    let password2 = $("#input-password2").val();
+signupBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  let username = $("#input-username").val();
+  let password = $("#input-password").val();
+  let password2 = $("#input-password2").val();
 
-    if ($("#help-id").hasClass("is-danger")) {
-      alert("아이디를 다시 확인해주세요.");
-      return;
-    } else if (!$("#help-id").hasClass("is-success")) {
-      alert("아이디 중복확인을 해주세요.");
-      return;
-    }
+  if ($("#help-id").hasClass("is-danger")) {
+    alert("아이디를 다시 확인해주세요.");
+    return;
+  } else if (!$("#help-id").hasClass("is-success")) {
+    alert("아이디 중복확인을 해주세요.");
+    return;
+  }
 
-    if (password == "") {
-      $("#help-password")
-        .text("비밀번호를 입력해주세요.")
-        .removeClass("is-safe")
-        .addClass("is-danger");
-      $("#input-password").focus();
-      return;
-    } else if (!is_password(password)) {
-      $("#help-password")
-        .text(
-          "비밀번호의 형식을 확인해주세요. 영문과 숫자 필수 포함, 특수문자(!@#$%^&*) 사용가능 8-20자"
-        )
-        .removeClass("is-safe")
-        .addClass("is-danger");
-      $("#input-password").focus();
-      return;
-    } else {
-      $("#help-password")
-        .text("사용할 수 있는 비밀번호입니다.")
-        .removeClass("is-danger")
-        .addClass("is-success");
-    }
-    if (password2 == "") {
-      $("#help-password2")
-        .text("비밀번호를 입력해주세요.")
-        .removeClass("is-safe")
-        .addClass("is-danger");
-      $("#input-password2").focus();
-      return;
-    } else if (password2 != password) {
-      $("#help-password2")
-        .text("비밀번호가 일치하지 않습니다.")
-        .removeClass("is-safe")
-        .addClass("is-danger");
-      $("#input-password2").focus();
-      return;
-    } else {
-      $("#help-password2")
-        .text("비밀번호가 일치합니다.")
-        .removeClass("is-danger")
-        .addClass("is-success");
-    }
+  if (password == "") {
+    $("#help-password")
+      .text("비밀번호를 입력해주세요.")
+      .removeClass("is-safe")
+      .addClass("is-danger");
+    $("#input-password").focus();
+    return;
+  } else if (!is_password(password)) {
+    $("#help-password")
+      .text(
+        "비밀번호의 형식을 확인해주세요. 영문과 숫자 필수 포함, 특수문자(!@#$%^&*) 사용가능 8-20자"
+      )
+      .removeClass("is-safe")
+      .addClass("is-danger");
+    $("#input-password").focus();
+    return;
+  } else {
+    $("#help-password")
+      .text("사용할 수 있는 비밀번호입니다.")
+      .removeClass("is-danger")
+      .addClass("is-success");
+  }
+  if (password2 == "") {
+    $("#help-password2")
+      .text("비밀번호를 입력해주세요.")
+      .removeClass("is-safe")
+      .addClass("is-danger");
+    $("#input-password2").focus();
+    return;
+  } else if (password2 != password) {
+    $("#help-password2")
+      .text("비밀번호가 일치하지 않습니다.")
+      .removeClass("is-safe")
+      .addClass("is-danger");
+    $("#input-password2").focus();
+    return;
+  } else {
+    $("#help-password2")
+      .text("비밀번호가 일치합니다.")
+      .removeClass("is-danger")
+      .addClass("is-success");
+  }
 
-    //  테스트 통과했음 저장해줘!
-    $.ajax({
-      type: "POST",
-      url: "/sign_up/save",
-      data: {
-        username_give: username,
-        password_give: password,
-      },
-      success: function (response) {
-        alert("회원가입을 축하드립니다!");
-        $("#chk").prop("checked", true);
-      },
-    });
+  //  테스트 통과했음 저장해줘!
+  $.ajax({
+    type: "POST",
+    url: "/sign_up/save",
+    data: {
+      username_give: username,
+      password_give: password,
+    },
+    success: function (response) {
+      alert("회원가입을 축하드립니다!");
+      $("#chk").prop("checked", true);
+      $("input").val("");
+      $("#help-id").removeClass("is-success");
+      $("#help-password").removeClass("is-success");
+      $("#help-password2").removeClass("is-success");
+    },
   });
+});
