8730bd75e6c9e6438d09dd1f9b14189b988f4d66
changmin97 <102282540+changmin97@users.noreply.github.com>
2022-05-11 12:04:21 +0900

    login 기능 구현

--- static/js/login.js
+++ static/js/login.js
@@ -4,6 +4,44 @@ function sign_up() {
     alert("회원가입!");
 }
 
-function log_in() {
-    alert("로그인!");
+function sign_in() {
+    let username = $("#input-username").val()
+    let password = $("#input-password").val()
+
+    if (username == "") {
+        alert("아이디를 입력해주세요.")
+        $("#input-username").focus()
+        return;
+    }
+    if (password == "") {
+        alert("비밀번호를 입력해주세요.")
+        $("#input-password").focus()
+        return;
+    }
+    $.ajax({
+        type: "POST",
+        url: "/sign_in",
+        data: {
+            username_give: username,
+            password_give: password
+        },
+        success: function (response) {
+            if (response['result'] == 'success') {
+                $.cookie('mytoken', response['token'], {path: '/'});
+                window.location.replace("/")
+            } else {
+                alert(response['msg'])
+            }
+        }
+    });
+}
+
+function is_nickname(asValue) {
+    var regExp = /^(?=.*[a-zA-Z])[-a-zA-Z0-9_.]{2,10}$/;
+    return regExp.test(asValue);
+}
+
+function is_password(asValue) {
+    var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,20}$/;
+    return regExp.test(asValue);
 }
